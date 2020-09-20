a5eb00b51062213f106c2bb1a4e0aaa3c1c3fb80
BOOMeranGG <bykov.e@notasoft.ru>
2020-09-20 15:09:41 +0300

    Init commit

---
+++ src/test/java/ru/notasoft/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.notasoft.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
